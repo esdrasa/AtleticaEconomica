bf85a562b2488a88d3455ff4542e9863b21c1278
vinicius <viniciusayres.sg@gmail.com>
2017-03-14 18:40:37 -0300

    Classe Item criada

---
+++ src/tabelaDispersao/Item.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabelaDispersao;
+
+/**
+ *
+ * @author Familia
+ */
+public class Item{
+    private long matricula;
+    private long posicao;
+    
+    public Item(long matricula, long posicao){
+        this.matricula = matricula;
+        this.posicao = posicao;
+    }
+    
+    public Item(){
+        this.matricula = -1;
+        this.posicao = -1;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public long getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the posicao
+     */
+    public long getPosicao() {
+        return posicao;
+    }
+
+    /**
+     * @param posicao the posicao to set
+     */
+    public void setPosicao(long posicao) {
+        this.posicao = posicao;
+    }
+    
+    
+}
